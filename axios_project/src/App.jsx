7e473909e0e7b4f110e80731afd61827f7d9b58f
Delta Student <yashikachahal512@gmail.com>
2025-10-17 00:43:22 +0530

    add files

---
+++ axios_project/src/App.jsx
@@ -0,0 +1,13 @@
+import { Posts } from "./components/Posts";
+export const App=()=>{
+
+  return (
+  <>
+    <section className="main-section">
+       <Posts/>
+    </section>
+   
+  </>
+  )
+}
+export default App;
